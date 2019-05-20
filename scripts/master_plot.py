d78f7a69bed48596aa06a8a0048a9c0c86e33e29
alegis277 <oa.diaz10@uniandes.edu.co>
2019-05-19 20:11:32 -0500

    Finished plot node for master

--- scripts/master_plot.py
+++ scripts/master_plot.py
@@ -10,8 +10,12 @@ import warnings
 import matplotlib.cbook
 warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
 from matplotlib import transforms
+from matplotlib.patches import Ellipse, Wedge
+import numpy as np
+import threading
 
-global X, Y, THETA
+global X, Y, THETA, trajectory_x, trajectory_y
+global sigma_matrix
 
 rospack = rospkg.RosPack()
 plt.rcParams["figure.figsize"] = (7,6)
@@ -25,7 +29,10 @@ frame_height=40
 X = 0
 Y = 0
 THETA = 0
-
+trajectory_x = []
+trajectory_y = []
+sigma_matrix = np.matrix([ [0,0,0],[0,0,0],[0,0,0] ])
+lock = threading.Lock()
 
 
 def kill_node():
@@ -33,17 +40,25 @@ def kill_node():
 
 
 def positionCallback(msg):
-	global X, Y, THETA
-	X = msg.position.x 
-	Y = msg.position.y
+	global X, Y, THETA, trajectory_x, trajectory_y
+
+	lock.acquire()
+	X = msg.position.x/10
+	Y = msg.position.y/10
 	THETA = msg.orientation.w
+	trajectory_x.append(X)
+	trajectory_y.append(Y)
+	lock.release()
 
 def uncertaintyCallback(msg):
-	pass
+	global sigma_matrix
+
+	sigma_matrix = np.matrix([ [msg.sigma11, msg.sigma12, msg.sigma13], [msg.sigma21, msg.sigma22, msg.sigma23], [msg.sigma31, msg.sigma32, msg.sigma33] ])
 
 def main_master_plot():
 
-	global X, Y, THETA
+	global X, Y, THETA, trajectory_x, trajectory_y
+	global sigma_matrix
 
 	rospy.loginfo("Starting master plot node")
 
@@ -52,7 +67,7 @@ def main_master_plot():
 	rospy.Subscriber('robot_uncertainty', Covariance, uncertaintyCallback)
 	
 	
-	rate = rospy.Rate(4)
+	rate = rospy.Rate(30)
 
 	if matplotlib.__version__ < '2.2.4':
 		rospy.logfatal("Update matplotlib to run the plot node: Version 2.2.4 is required")
@@ -77,14 +92,23 @@ def main_master_plot():
 	win.geometry("+{}+{}".format(positionRight, positionDown))
 
 	plt.show()
-
 	addedImg = False
 
 	while not rospy.is_shutdown():
+
+		x_unc = np.sqrt(sigma_matrix[0,0]) / 10
+		y_unc = np.sqrt(sigma_matrix[1,1]) / 10
+		theta_unc = np.sqrt(sigma_matrix[2,2]) * 180/np.pi
 		
 		if addedImg:
 			robotImgMPL.remove()
+			trajectory.remove()
+			oval_uncertainty.remove()
+			wedge_uncertainty.remove()
 			del robotImgMPL
+			del trajectory
+			del oval_uncertainty
+			del wedge_uncertainty
 
 		robotImgMPL = ax1.imshow(img, zorder=10)
 
@@ -92,15 +116,31 @@ def main_master_plot():
 		tr.rotate_around(132*0.3/2, 132*0.3/2, THETA)
 		tr.translate(X-132*0.3/2, Y-132*0.3/2)
 
-
 		tr = tr + ax1.transData
 		robotImgMPL.set_transform(tr)
+
+		lock.acquire()
+		trajectory, = ax1.plot(trajectory_x, trajectory_y, c='firebrick')
+		lock.release()
+
+		oval_uncertainty = Ellipse((0,0), width=x_unc*2, height=y_unc*2, facecolor='lightblue', edgecolor='midnightblue', alpha=0.3, zorder=20)
+		tr_oval = transforms.Affine2D().translate(X, Y) #rotate(THETA)
+		oval_uncertainty.set_transform(tr_oval + ax1.transData)
+		ax1.add_patch(oval_uncertainty)
+
+		wedge_uncertainty = Wedge((0,0), 45, theta1=-theta_unc, theta2=theta_unc, zorder=5, color='cornflowerblue', alpha=0.5)
+		tr_wedge = transforms.Affine2D().rotate(THETA).translate(X, Y)
+		wedge_uncertainty.set_transform(tr_wedge + ax1.transData)
+		ax1.add_patch(wedge_uncertainty)
+
+		
 
 		addedImg = True
 		plt.draw()
 		plt.pause(0.001)
 
 		rate.sleep()
+
 
 
 if __name__ == '__main__':
